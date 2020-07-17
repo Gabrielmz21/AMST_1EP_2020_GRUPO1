82a12b24e749354017e5930bce3b5e99446e4174
Eileen <eileen_sanchez@hotmail.es>
2020-07-17 11:06:24 -0500

    se creo proyecto en android

---
+++ app/src/main/java/com/grupo2/amst_1ep_2020_grupo1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grupo2.amst_1ep_2020_grupo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
